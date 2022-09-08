b8fd63ec6c6855ef5dbd9d59d8d3617298a8193c
Thien Nguyen <104371084+ThienNguyenThanh@users.noreply.github.com>
2022-09-08 15:22:12 +0700

    Update check_duplicate.py

--- Arrays/check_duplicate.py
+++ Arrays/check_duplicate.py
@@ -1,1 +1,6 @@
-
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        set_len = len(set(nums))
+        list_len = len(nums)
+        if set_len != list_len: return True
+        else: return False
