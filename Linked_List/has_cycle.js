c50deec5e34a7c0ada789b54edf18e0c1a96a878
Thien Nguyen <104371084+ThienNguyenThanh@users.noreply.github.com>
2022-11-09 17:56:58 +0700

    Create has_cycle.js

---
+++ Linked_List/has_cycle.js
@@ -0,0 +1,15 @@
+var hasCycle = function(head) {
+    if(head == null) return false
+    var slow = head
+    var fast = head
+    while(fast != null && fast.next != null){
+        slow = slow.next
+        fast = fast.next.next
+        if(slow == fast){
+            return true
+        }
+    }
+    
+    return false
+    
+};
