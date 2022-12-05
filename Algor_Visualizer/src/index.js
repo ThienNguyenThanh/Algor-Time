c06e8e19e36b91c5562c8154486a9a9d8da85e12
Thien Thanh <nguyenthanhthien0611@gmail.com>
2022-12-05 11:23:29 +0700

    install styled-components

--- Algor_Visualizer/src/index.js
+++ Algor_Visualizer/src/index.js
@@ -6,9 +6,12 @@ import SortingVisualizer from './components/SortingVisualizer';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <SortingVisualizer />
-  </React.StrictMode>
+  // <React.StrictMode>
+  //   <SortingVisualizer />
+  // </React.StrictMode>
+  <div>
+    <button>Click me</button>
+  </div>
 );
 
 // If you want to start measuring performance in your app, pass a function
